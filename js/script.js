60d68fc6db3aea1daac6a0fedd1da2fa62252e44
Juan Cirilo <juancirilo194@yahoo.com>
2018-02-28 22:45:40 +0000

    whitespace

--- js/script.js
+++ js/script.js
@@ -17,6 +17,8 @@ var myPlayList = [{
 		"mp3-url": "https://open.spotify.com/track/09CtPGIpYB4BrO8qb1RGsF",
 		"image-url": "http://assets-s3.usmagazine.com/uploads/assets/articles/93827-justin-biebers-sorry-choreographer-spills-video-style-secrets-parris-goebel/1445638548_justin-bieber-sorry-dancers-zoom.jpg",
 	}
+	
+	
 ];
 
 
